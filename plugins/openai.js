1818a2131aa3b382a1b76c3287bc2df46609234e
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2023-01-24 09:18:01 +0800

    Update openai.js

--- plugins/openai.js
+++ plugins/openai.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 let handler = async (m, { conn, text }) => {
 if (!text) throw "[!] Masukkan teks."
 const configuration = new Configuration({
-    apiKey: ""
+    apiKey: "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"
 });
 const openai = new OpenAIApi(configuration);
         const response = await openai.createCompletion({
